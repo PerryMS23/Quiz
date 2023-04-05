c272d6d52ec1e47d8f9cb21d4cfd3c8b9da27c50
PerryMS23 <126625740+PerryMS23@users.noreply.github.com>
2023-04-06 09:36:24 +1200

    Add files via upload

---
+++ Quiz/MultipleChoiceQuestion.py
@@ -0,0 +1,8 @@
+while True:
+    d1a = input("What colour is an apple?: \n A) Red. B) Blue. [A/B]? : ")
+    if d1a == "A":
+        print("A is correct!")
+        break
+    elif d1a == "B":
+        print("B is incorrect!")
+        break
