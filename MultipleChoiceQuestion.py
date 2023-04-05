c2051267d5bd8629eae8bb485b1dcf5c23146817
PerryMS23 <126625740+PerryMS23@users.noreply.github.com>
2023-04-06 09:36:42 +1200

    Delete MultipleChoiceQuestion.py

--- MultipleChoiceQuestion.py
+++
@@ -1,8 +0,0 @@
-while True:
-    d1a = input("What colour is an apple?: \n A) Red. B) Blue. [A/B]? : ")
-    if d1a == "A":
-        print("A is correct!")
-        break
-    elif d1a == "B":
-        print("B is incorrect!")
-        break
